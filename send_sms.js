7a5f580aee13f55a8d072ed0a5f1d4f6e41cd861
Ryan Zhen <rjhzhen@gmail.com>
2023-01-08 01:23:34 -0500

    Initial Commit

---
+++ send_sms.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'Hello I am testing my Twilio app',
+     from: process.env.SEND_FROM_NUMBER,
+     to: process.env.SEND_TO
+   })
+  .then(message => console.log(message.sid));
